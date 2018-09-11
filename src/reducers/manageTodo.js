28871bb0bcba4477d6e84684611315a42aa9fb3a
Cristina Lugo <cmlugoce@syr.edu>
2018-09-11 14:58:04 +0000

    automatically backed up by learn

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,18 +1,19 @@
 export default function manageTodo(state = {
   todos: [],
 }, action) {
+  console.log(action);
   switch (action.type) {
     case 'ADD_TODO':
 
-    
+
           const todo = {
             id: Math.random()*10000000000000000,
             text: action.text
           }
           return {...state, bands: [...state.bands, todo]};
-     
+
         case 'DELETE_TODO':
-     
+
           return {todos: state.todos.filter(todo => todo !== action.id)}
     default:
       return state;
